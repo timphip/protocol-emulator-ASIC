4253303d7d6944cd05f219d4c05dccd0f8f34a21
Tim Zheng <yt209241@dal.ca>
2026-09-12 19:00:31 -0300

    init

---
+++ get_start/tb_and_gate.v
@@ -0,0 +1,24 @@
+module tb_and_gate;
+
+reg a;
+reg b;
+wire y;
+
+and_gate dut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    $monitor("time=%0t a=%b b=%b y=%b", $time, a, b, y);
+
+    a = 0; b = 0; #10;
+    a = 0; b = 1; #10;
+    a = 1; b = 0; #10;
+    a = 1; b = 1; #10;
+
+    $finish;
+end
+
+endmodule
